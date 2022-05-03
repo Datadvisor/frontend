1055ae3c3b6434c38d523dce772e8df36451ae48
Roman Gascoin <54111474+Rgascoin@users.noreply.github.com>
2022-05-03 10:31:43 +0200

    feat: add contact page and form with errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grape: colors.purple,
+      },
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
 }
